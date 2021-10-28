f1ff2153f80482425dc0b6ab6ec6c52c9e306b4f
Andrea Parisi <andreaparisi4502@gmail.com>
2021-10-28 16:14:48 +0200

    added ex3

---
+++ jsnack3/script.js
@@ -0,0 +1,18 @@
+/*
+    3 - Il software deve chiedere per 10 volte all’utente di inserire un numero.
+    Il programma stampa la somma di tutti i numeri inseriti.
+
+    1. chiedo all'utente i dieci numeri
+    2. sommo i numeri
+    3. stampo il risultato
+
+*/
+
+let numeri = []
+let risultato = 0;
+
+for(let i = 0; i < 10; i++){
+    numeri[i] = parseInt(prompt("inserisci un numero: ", numeri[i]));
+    risultato += numeri[i]; 
+}
+console.log("risultato: ", risultato);
